696744e4b30fd3b049068bd856bb1aed87c2def6
Rabbo <18214304@sun.ac.za>
2018-08-17 22:40:13 +0200

    got the bits working bro

--- Tut2_Receiver/src/RBUDP_Thread.java
+++ Tut2_Receiver/src/RBUDP_Thread.java
@@ -1,3 +1,64 @@
-public class RBUDP_Thread {
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.DatagramPacket;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.rmi.CORBA.Util;
+
+public class RBUDP_Thread extends Thread{
+
     
+            
+    public RBUDP_Thread() {
+        
+        
+    }
+    
+    @Override
+    public void run(){
+         int count = 0;
+
+        while(true){
+            try {
+
+                byte[] tempBytes = new byte[ReceiverRBUDP.packetSize+4];
+                DatagramPacket temp = new DatagramPacket(tempBytes, tempBytes.length);
+                ReceiverRBUDP.socket.receive(temp);
+                ReceiverRBUDP.packetlist.add(temp);
+                byte[] seq = new byte[4];
+                System.arraycopy(temp.getData(), 0, seq, 0, 4);
+                int seqI = toInt(seq);
+                
+                
+                
+                System.err.println(seqI);
+                count++;    
+                System.out.println(count +" COUNT ===========");
+                if(count%5 == 0){
+                    
+                }
+            } catch (Exception ex) {
+                System.err.println("exception in RBUDP_thread : " + ex);
+                
+              }
+            
+            
+        }
+    
+    }
+   
+    
+
+    public int convertByteToInt(byte[] b){           
+        int value= 0;
+        for(int i=0; i<b.length; i++)
+           value = (value << 4) | b[i];     
+        return value;       
+    }
+    
+     public int toInt(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getInt();
+     }
 }
--- Tut2_Receiver/src/ReceiverRBUDP.java
+++ Tut2_Receiver/src/ReceiverRBUDP.java
@@ -1,37 +1,64 @@
 
+import java.io.File;
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ReceiverRBUDP {
+    public static String filename ;
+    public static int filesize;
+    static int port = 7999;
+    public static int packetSize = 5;
+    public static DatagramSocket socket = null;
+    public static DatagramPacket packet;
+    static public byte[] fileAsBytes;
+    static public ArrayList<DatagramPacket> packetlist = new ArrayList<DatagramPacket>();
     
-    int port = 7999;
-    DatagramSocket socket = null;
-    DatagramPacket packet;
     
     public ReceiverRBUDP() {
         receiveFile();
     }
     
-    public void receiveFile(){
+    public static void receiveFile(){
+        
+        setName_Size();
+        String cwd = System.getProperty("user.dir");
+        File file = new File(cwd+"/"+filename);
+        fileAsBytes = new byte[filesize];
+        
         try {
             socket = new DatagramSocket(port);
-            System.out.println("a");
-            byte[] buffer = new byte[10000];
-            packet = new DatagramPacket(buffer, buffer.length);
-            System.out.println("b");
-            socket.receive(packet);
-            System.out.println("c");
-            String received = new String(packet.getData());
-            System.out.println("d");
-            System.out.println("received : " + received);
+            RBUDP_Thread t = new RBUDP_Thread();
+            System.out.println("line 37 Receiver_RBUDP");
+
+            t.start();
+            
+            //System.out.println(t.getName());
+            //byte[] buffer = new byte[10000];
+            //packet = new DatagramPacket(buffer, buffer.length);
+            //socket.receive(packet);
+            //String received = new String(packet.getData());
             
         } catch (Exception e) {
             System.err.println("could not create datagram socket : " + e);
         }
     }
     
+    public static void setName_Size(){
+        try {
+            filename = Receiver.in.readUTF();
+            filesize = Receiver.in.readInt();
+            System.out.println("filename = " + filename + "    FileSize = " + filesize);
+
+        } catch (IOException ex) {
+            Logger.getLogger(ReceiverRBUDP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
+    
+    }
 }
